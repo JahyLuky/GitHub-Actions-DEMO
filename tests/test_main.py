169f83b9755bb647bf06aa7fbdd128d980e2d44c
Lukáš Jahelka <94187567+JahyLuky@users.noreply.github.com>
2025-11-03 16:05:28 +0100

    feat: Add additional tests for hello function

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,3 +2,9 @@ from src.main import hello
 
 def test_hello():
     assert hello("Paul") == "Hello, Paul!"
+
+def test_hello_empty():
+    assert hello("") == "Hello, !"
+
+def test_hello_none():
+    assert hello(None) == "Hello, None!"
